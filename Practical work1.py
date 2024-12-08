a6dccd5f179ab8a603c0a9105e6325f49197549e
Clockingthatteawithmamadatburnhoe2024 <matvei.gres@gmail.com>
2024-12-08 18:45:47 +0300

    Add files via upload

---
+++ Practical work1.py
@@ -0,0 +1,51 @@
+# Cheking if you are older then 18
+
+#age = int(input("Diva, wrtie your age please ")) #age
+#if age >= 18: 
+#    product = input("Beer or vodka? it's on the house.")
+#    if product == "Beer":
+#        print("now you gotta drink the whole bottle")
+#    elif product == "Vodka":
+#        print("you gotta drink two bottles now, you thought you were old? get that in yo face")
+
+#elif age < 18:
+#    print("Oop, not allowed, you are too young, GET OUT!")
+
+#else:
+#    print("that's not even a number")
+
+#test = False
+
+#if test:
+#    print("correct for 1")
+#    print("correct for 2")
+#    print("correct for 3")
+
+#print("}Menu{")
+#print("1. Plus\n 2. Minus\n 3. multiplying\n 4. Dividing\n 5. exit")
+#choice = int(input(""))
+
+#a = int(input("Write the number a "))
+#b = int(input("Write the number b "))
+
+#if choice == 1:
+#    c = a + b
+#    print("by mat You've got a number", c)
+
+#if choice == 2:
+#    c = a - b
+#    print("by mat You've got a number", c)
+
+#if choice == 3:
+#    c = a * b
+#    print("by mat You've got a number", c)
+
+#if choice == 4:
+#    c = a / b
+#    print("by mat You've got a number", c)
+
+number = int(input("Write the number "))
+
+if number > 10 and number < 20:
+    print("The written number is less then 20 and more then 10")
+
---
+++ practical work.py
@@ -0,0 +1,10 @@
+number = int(input("Write the number "))
+
+if number > 0:
+    print("Your number is more then 0, Congrats Diva! you are so good at this game, wanna play another game? if yes, press start again, if no, GET OUT!")
+
+elif number < 0:
+    print("Choose the number more then 0, you need it to be more then 0, you loose, how could you even loose, that's literally so easy")
+
+else:
+    print("You wrote either 0 or not even a number, try putting a number that would help you in this game, and your forehead is big, you could fit all peaople in the world on it")
